1ac26fa853135d36b357ab58f385b487b019b5e6
das-Sumyadeep <133164855+das-Sumyadeep@users.noreply.github.com>
2024-02-13 16:47:27 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: { min: "320px", max: "374px" }, // Msmobile
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        sm: { min: "375px", max: "538px" }, // Lmobile
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        md: { min: "539px", max: "739px" }, // Mtablet
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        xm: { min: "740px", max: "1023px" }, // Ltablet
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+        lg: { min: "1024px", max: "1279px" }, // Mlaptop
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+        xl: { min: "1280px", max: "" }, // Llaptop
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+      },
+    },
   },
   plugins: [],
-
-}
+};
